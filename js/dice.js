96b6e94eb4bee2c208d791e52ad54868d8c0039c
LincolnTunapanda <lincolnajanga@gmail.com>
2018-03-05 07:41:20 +0300

    Added jquery and javascript logic

--- js/dice.js
+++ js/dice.js
@@ -1,1 +1,29 @@
 //Pig dice logic
+
+//Javascript welcome logic
+
+var welcomeToPigDice = function(){
+	var result = ""
+	if (userInput ! = " ") {
+		result.push('Welcome'+' '+userInput+' '+'to pigDice!'); 
+	}
+	else{
+		result.push('Please retype your name and try again!');
+	}
+
+return result;
+}
+
+
+$(document).ready(function(){
+	$('#pigster2').click(function(event){
+		event.preventDefault();
+
+		//getting input from the user
+		var userInput = $('#pigster1').val();
+		$('#karibuSana').empty();
+		var result = welcomeToPigDice(userInput);
+$('#karibuSana').text(result);
+	})
+});
+
